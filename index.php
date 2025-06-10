51fe6360502112c22c146ecbd4e2b328afff9111
kgchagas <kaueguilhermefc@gmail.com>
2025-06-09 21:39:57 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Chewy&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+    <title>Espaço Rennova </title>
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+</head>
+<body>
+    <nav>
+        <div class="navbar-container">
+            <a href="index.php" class="navbar-logo">
+                <img src="logo.png" alt="Logo"> Espaço Rennova
+            </a>
+            <ul class="navbar-links">
+                <li><a href="#servicos">Serviços</a></li>
+                <li><a href="#contato">Contato</a></li>
+                <li><a href="#login">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+    <H1>Massoterapia no Espaço Rennova</H1>
+    <h2>Cuidando do seu corpo, mente e bem-estar.</h2>
+    No Espaço Rennova, oferecemos uma experiência completa em massoterapia, 
+    com foco no equilíbrio físico e emocional.
+     Contamos com profissionais especializados e técnicas modernas para proporcionar alívio,
+      estética e saúde
+</body>
+</html>
